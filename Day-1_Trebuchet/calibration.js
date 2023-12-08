1dc20e9d17d48956cd2bd4308bdb806cb98e696d
Hamilton <>
2023-12-08 19:49:01 +0100

    refactor

--- Day-1_Trebuchet/calibration.js
+++ Day-1_Trebuchet/calibration.js
@@ -12,7 +12,7 @@ const numbersInLetters = [
   { nine: 9 },
 ];
 
-// Iterate through the array and replace words with their numeric values
+// Replace words with their numeric values
 function replaceNumericValues(inputString) {
   numbersInLetters.forEach((numberObj) => {
     const word = Object.keys(numberObj)[0];
@@ -27,41 +27,38 @@ function replaceNumericValues(inputString) {
   return inputString;
 }
 
-let lineNumbers;
-let lineResult;
-
+// Read the file and process the data
 fs.readFile("calibration-value.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  // Splitting the data into an array of strings
+  // Split the data into an array of strings
   const lines = data.split("\n");
 
-  // Retreiving the numbers from each line
-  lineNumbers = lines.map((line) => {
+  // Retrieve the numbers from each line
+  const lineNumbers = lines.map((line) => {
     const replacedNumbers = replaceNumericValues(line);
     const allLineNumbers = replacedNumbers.match(/\d+/g).map(Number);
     const allDigits = allLineNumbers.join("").split("").map(Number);
     return allDigits;
   });
-  console.log("lineNumbers", lineNumbers);
 
-  // Concatenating the numbers from each line
-  lineResult = lineNumbers.map((line) => {
-    let a = line[0].toString();
-    let b = line[line.length - 1].toString();
-    if (line.length - 1 === 0) {
-      return a + a;
-    }
-    return a + b;
-  });
-  lineResult = lineResult.map(Number);
-  console.log("lineResult", lineResult);
+  // Concatenate the numbers from each line
+  const lineResult = lineNumbers
+    .map((line) => {
+      const a = line[0].toString();
+      const b = line[line.length - 1].toString();
+      if (line.length - 1 === 0) {
+        return a + a;
+      }
+      return a + b;
+    })
+    .map(Number);
 
-  // Adding up the final result
+  // Add up the final result
   const finalResult = lineResult.reduce((a, b) => a + b, 0);
 
-  console.log("finalResult", finalResult);
+  console.log("finalResult:", finalResult);
 });

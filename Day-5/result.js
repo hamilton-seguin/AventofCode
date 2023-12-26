4779a91904d11d45113a6e37754a9fa5fd43ecbc
hamilton-seguin <hamilton.seguin@gmail.com>
2023-12-26 18:57:28 +0100

    add solution part 1, not refactored

--- Day-5/result.js
+++ Day-5/result.js
@@ -1,97 +1,95 @@
 import fs from "fs";
-import { nextTick } from "process";
 
-fs.readFile("test.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+const data = fs.readFileSync("data.txt", { encoding: "utf8" });
 
-  const lines = data.split("\n");
-  // console.log("lines", lines);
+const lines = data.split("\n");
+// console.log("lines", lines);
 
-  /**
-   * Destination - Source - Range
-   * const
-   * if (seed >= source && seed <= range + source { return seed + (destination - source)}
-   * else continue
-   *
-   * Create new array for each map
-   * create a function that takes in a seed and goes through each array
-   */
+const seedList = lines[0].split(": ").splice(1)[0].split(" ").map(Number);
+console.log("seedList", seedList);
 
-  const seedList = lines[0].split(": ").splice(1, 1)[0].split(" ").map(Number);
-  // console.log("seedList", seedList);  // seedList [ 79, 14, 55, 13 ]
+/** Cut the data by map */
+let currentMap = [];
+let allMaps = [];
 
-  /** Cut the data by map */
-  let currentMap = [];
-  let allMaps = [];
-
-  lines.forEach((line) => {
-    if (line.trim() !== "") {
-      currentMap.push(line);
-    } else {
-      if (currentMap.length > 0) {
-        allMaps.push(currentMap);
-        currentMap = [];
-      }
+lines.forEach((line) => {
+  if (line.trim() !== "") {
+    currentMap.push(line);
+  } else {
+    if (currentMap.length > 0) {
+      allMaps.push(currentMap);
+      currentMap = [];
     }
-  });
-  if (currentMap.length > 0) {
-    allMaps.push(currentMap);
   }
-
-  function removeText(str) {
-    return str.splice(0, 1);
-  }
-  function numbersToArray(str) {
-    const newArray = str.map((str) => {
-      return str.split(" ");
-    });
-    return newArray;
-  }
-
-  // allMaps = allMaps.splice(1, allMaps.length - 1)
-  removeText(allMaps);
-  // console.log("allMaps", allMaps);
-
-  const newMap = allMaps.map((map) => {
-    const noText = map.splice(1, allMaps.length - 1);
-    const numbersArray = numbersToArray(noText);
-    return numbersArray;
-  });
-  // console.log("newMap", newMap);
-
-  const fakeSeeds = [79]
-  const fakeMaps = [[[50, 98, 2], [52, 50, 48]]]
-  function findLocation(seedArray, mapArray) {
-    seedArray.forEach((seed) => {
-      mapArray.map((map) => {
-        console.log("each map", map);
-        map.forEach((map) => {
-          const [destination, source, range] = map;
-          console.log("map", map);
-          console.log("seed", seed);
-          if (seed >= source && seed <= ((range-1) + source)) {
-            const aToB = seed + (destination - source);
-            console.log("A2B", aToB);
-            return aToB
-          }
-          console.log("not this map");
-          if (seed < source) return seed;
-
-        });
-      });
-    });
-  }
-
-  // findLocation(seedList, newMap);
-  findLocation(fakeSeeds, fakeMaps);
-
-  // const seetToSoil = ["blabla", "50 98 2", "52 50 48"];
-  // function findLocation(seedArray) {
-  //   seedArray.map((seed) => {
-  //     allMaps.forEach((map) => {});
-  //   });
-  // }
 });
+if (currentMap.length > 0) {
+  allMaps.push(currentMap);
+}
+
+function removeText(str) {
+  return str.splice(0, 1);
+}
+function numbersToArray(str) {
+  const newArray = str.map((str) => {
+    const string = str.split(" ");
+    return string.map(Number);
+  });
+  return newArray;
+}
+
+// allMaps = allMaps.splice(1, allMaps.length - 1)
+removeText(allMaps);
+console.log("allMaps", allMaps);
+
+const newMap = allMaps.map((map, i) => {
+  const noText = map.splice(1, allMaps[i].length - 1);
+  const numbersArray = numbersToArray(noText);
+  return numbersArray;
+});
+console.log("newMap", newMap);
+
+function findLocation(seedArray, mapArray) {
+  let seedsLocation = [];
+  seedArray.forEach((seed) => {
+    // console.log( "\n", "NEW SEED", seed);
+    let currentSeed;
+    mapArray.map((map, i) => {
+      if (i === 0) currentSeed = seed;
+      const mapLength = map.length;
+      // console.log("next group of maps", map);
+      // console.log("currentSeed", currentSeed);
+      // console.log("mapLength", mapLength);
+      let foundIt;
+      map.forEach((map, i) => {
+        if (i === 0) foundIt = false;
+        if (foundIt === true) return;
+        const [destination, source, range] = map;
+        // console.log("currentSeed in map", currentSeed);
+        // console.log("map", map);
+        if (currentSeed >= source && currentSeed <= range - 1 + source) {
+          const aToB = currentSeed + (destination - source);
+          // console.log("A2B", aToB);
+          foundIt = true;
+          // console.log("foundIt", foundIt);
+          return (currentSeed = aToB);
+        }
+        // console.log("not this map");
+        if (i === mapLength - 1 && !foundIt) {
+          // console.log("nextSeed = currentSeed", currentSeed);
+          return currentSeed;
+        }
+      });
+      if (i === mapArray.length - 1) {
+        // console.log("final currentSeed", currentSeed);
+        return seedsLocation.push(currentSeed);
+      }
+    });
+  });
+  return seedsLocation;
+}
+
+const result = findLocation(seedList, newMap);
+console.log("result", result);
+
+const closestLocation = Math.min(...result);
+console.log("closestLocation", closestLocation);
